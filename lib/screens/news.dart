1e11d5b2fff083b1aae9f5c2a96d58054f746d39
Ondřej Škoda <comancheo@seznam.cz>
2026-02-11 22:55:38 +0100

    implement loading to map and news

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,9 +1,12 @@
+import 'package:comancheo_cv/cubits/base_cubits.dart';
 import 'package:comancheo_cv/services/ctk_news.dart';
 import 'package:comancheo_cv/widgets/custom_scaffold.dart';
 import 'package:comancheo_cv/widgets/news_item_card.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rss_dart/domain/rss_item.dart';
 
 @RoutePage()
 class NewsScreen extends StatefulWidget {
@@ -17,23 +20,33 @@ class NewsScreenState extends State<NewsScreen> {
   final CtkNewsService _ctkNewsService = GetIt.instance<CtkNewsService>();
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      primary: false,
-      onRefresh: () async {
-        return await _ctkNewsService.loadNews();
+    return BlocBuilder<NullBoolCubit, bool?>(
+      bloc: _ctkNewsService.loading,
+      builder: (context, loading) {
+        return BlocBuilder<ListCubit<RssItem>, List<RssItem>>(
+          bloc: _ctkNewsService.newsItems,
+          builder: (context, items) {
+            return CustomScaffold(
+              primary: false,
+              onRefresh: () async {
+                return await _ctkNewsService.loadNews();
+              },
+              title: "Zprávy ČTK",
+              body: [
+                if (loading == true) ...[
+                    const Center(child: CircularProgressIndicator()),
+                  ] else if (items.isEmpty) ...[
+                    const SizedBox(),
+                  ] else
+                ...List.generate(_ctkNewsService.newsItems.state.length, (index) {
+                  final item = _ctkNewsService.newsItems.state[index];
+                  return NewsItemCard(title: item.title ?? '', description: item.description ?? '', imageUrl: item.enclosure?.url, link: item.link);
+                }),
+              ],
+            );
+          },
+        );
       },
-      title: "Zprávy ČTK",
-      body: [
-        ...List.generate(_ctkNewsService.newsItems.state.length, (index) {
-          final item = _ctkNewsService.newsItems.state[index];
-          return NewsItemCard(
-            title: item.title??'',
-            description: item.description??'',
-            imageUrl: item.enclosure?.url,
-            link: item.link,
-          );
-        }),
-      ],
     );
   }
 }
--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,5 +1,7 @@
+import 'package:comancheo_cv/cubits/base_cubits.dart';
 import 'package:comancheo_cv/utils/geolocation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get_it/get_it.dart';
@@ -16,52 +18,92 @@ class _MyMapState extends State<MyMap> {
   Position currentPosition = GeolocationService.fallbackPosition;
   GeolocationService? geolocationService;
   final MapController mapController = MapController();
+  bool mapReady = false;
   @override
   void initState() {
     super.initState();
     GetIt.instance.getAsync<GeolocationService>().then((service) {
       setState(() {
         geolocationService = service;
-        currentPosition = geolocationService?.currentPosition ?? GeolocationService.fallbackPosition;
-        mapController.moveAndRotate(currentPosition.latLng, 9.2, 0);
+        setPositionAndState();
+      });
+      geolocationService!.loading.stream.listen((loading) {
+        if (loading == false) {
+          setPositionAndState();
+        }
       });
     });
   }
+
+  void setPositionAndState() {
+    if (geolocationService != null && mapReady) {
+      setState(() {
+        setPosition();
+      });
+    }
+  }
+
+  void setPosition() {
+    if (geolocationService != null && mapReady) {
+      currentPosition = geolocationService?.currentPosition ?? GeolocationService.fallbackPosition;
+      mapController.moveAndRotate(currentPosition.latLng, 9.2, 0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              initialCenter: currentPosition.latLng,
-              initialZoom: 9.2,
+    if (geolocationService == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return BlocBuilder<NullBoolCubit, bool?>(
+      bloc: geolocationService!.loading,
+      builder: (context, loading) {
+        return FlutterMap(
+          mapController: mapController,
+          options: MapOptions(
+            initialCenter: currentPosition.latLng,
+            initialZoom: 9.2,
+            onMapReady: () {
+              setState(() {
+                mapReady = true;
+                setPosition();
+              });
+            },
+          ),
+          children: [
+            TileLayer(
+              // Bring your own tiles
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+              userAgentPackageName: 'cz.comancheo.cv', // Add your app identifier
+              // And many more recommended properties!
             ),
-            children: [
-              TileLayer(
-                // Bring your own tiles
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
-                userAgentPackageName: 'cz.comancheo.cv', // Add your app identifier
-                // And many more recommended properties!
-              ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: currentPosition.latLng, // Location of the marker
-                    child: GestureDetector(
-                      onTap: () async {
-                        String url = 'https://www.google.com/maps/place/${currentPosition.latitude},${currentPosition.longitude}';
-                        await launchUrl(Uri.parse(url));
-                      },
-                      child: Icon(Icons.location_on, color: Theme.of(context).colorScheme.primary, size: 40),
-                    ),
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: currentPosition.latLng, // Location of the marker
+                  child: GestureDetector(
+                    onTap: () async {
+                      String url = 'https://www.google.com/maps/place/${currentPosition.latitude},${currentPosition.longitude}';
+                      await launchUrl(Uri.parse(url));
+                    },
+                    child: Icon(Icons.location_on, color: Theme.of(context).colorScheme.primary, size: 40),
                   ),
-                ],
-              ),
-              ///get my location button
-              IconButton(onPressed: () async {
+                ),
+              ],
+            ),
+
+            ///get my location button
+            IconButton(
+              onPressed: () async {
                 mapController.moveAndRotate(geolocationService?.currentPosition?.latLng ?? GeolocationService.fallbackPosition.latLng, 9.2, 0);
                 //get my location
-              }, icon: Icon(Icons.my_location))
-            ],
-          );
+              },
+              icon: Icon(Icons.my_location),
+            ),
+            if (loading == true) SizedBox(child: const Center(child: CircularProgressIndicator())),
+          ],
+        );
+      },
+    );
   }
 }
